1621cb6ec395451f56ba6ae69ae4e46d6bdc6ad3
gisserel <guillaume.isserel@mypangee.com>
2025-01-21 16:32:50 +0100

    feat(custom): ✨ Add http-request service
    ISSUES CLOSED: #18

---
+++ src/app/core/utils/services/http-request.service.spec.ts
@@ -0,0 +1,17 @@
+import { TestBed } from '@angular/core/testing';
+
+import { HttpRequestService } from './http-request.service';
+import { HttpClientModule } from '@angular/common/http';
+
+describe('HttpRequestService', () => {
+  let service: HttpRequestService;
+
+  beforeEach(() => {
+    TestBed.configureTestingModule({ imports: [HttpClientModule] });
+    service = TestBed.inject(HttpRequestService);
+  });
+
+  it('should be created', () => {
+    expect(service).toBeTruthy();
+  });
+});
