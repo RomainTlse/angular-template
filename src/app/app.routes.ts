8357e4586e75f6edc741a3a476ac85c9a4f4aba5
gisserel <guillaume.isserel@mypangee.com>
2025-02-26 10:32:04 +0100

    fix(custom): 🐛 Add url for login

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -3,6 +3,10 @@ import { PageNotFoundComponent } from './core/ui/pages/page-not-found/page-not-f
 import { PageUnauthorizedComponent } from './core/ui/pages/page-unauthorized/page-unauthorized.component';
 import { TestComponent } from './test/test.component';
 import { NotificationComponent } from './core/ui/pages/notification/notification.component';
+import { LoginComponent } from './core/ui/pages/login/login/login.component';
+import { SignUpComponent } from './core/ui/pages/login/sign-up/sign-up.component';
+import { ForgotPwdComponent } from './core/ui/pages/login/forgot-pwd/forgot-pwd.component';
+import { NewPwdComponent } from './core/ui/pages/login/new-pwd/new-pwd.component';
 
 export const routes: Routes = [
   {
@@ -12,6 +16,22 @@ export const routes: Routes = [
   {
     path: 'notification',
     component: NotificationComponent,
+  },
+  {
+    path: 'login',
+    component: LoginComponent,
+  },
+  {
+    path: 'sign-up',
+    component: SignUpComponent,
+  },
+  {
+    path: 'forgot-password',
+    component: ForgotPwdComponent,
+  },
+  {
+    path: 'new-password',
+    component: NewPwdComponent,
   },
   {
     path: 'page-unauthorized',
